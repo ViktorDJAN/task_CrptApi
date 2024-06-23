6641b410cdbed292ada3463b7aaa557fe7e443d0
viktor <pistoletviktor@gmal.com>
2024-06-23 15:05:20 +0300

    serialization is added

--- src/main/java/ru/kashtanov/Main.java
+++ src/main/java/ru/kashtanov/Main.java
@@ -1,16 +1,29 @@
 package ru.kashtanov;
 
+import com.google.gson.Gson;
 import ru.kashtanov.crpt.CrptApi;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         CrptApi crptApi1 = new CrptApi(TimeUnit.SECONDS, 1);
-        CrptApi.Document document = new CrptApi.Document();
-        crptApi1.createDocument(document,"Sd");
+        String stringForJson = "";
+
+        try{
+            byte[] bytes = Files.readAllBytes(Paths.get("src/main/resources/json_doc.json"));
+            stringForJson = new String(bytes, StandardCharsets.UTF_8);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        CrptApi.Document document1 = new Gson().fromJson(stringForJson, CrptApi.Document.class);
+        crptApi1.createDocument(document1,"Sd");
 
 
     }
